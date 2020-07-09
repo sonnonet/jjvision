ccab32180e9a19872b4df6645a494b2d99be2bf3
qo7860vm <qo7860vm@naver.com>
2020-07-09 15:12:35 +0900

    upload

--- pir.py
+++ pir.py
@@ -2,18 +2,42 @@
 
 import time
 import RPi.GPIO as GPIO
-
+import requests, json
+from influxdb import InfluxDBClient as influxdb
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN)
 
 def interrupt_fired(channel):
     print("interrupt Fired")
-    print(channel)
-
+    a = 1
+    print(a)
 GPIO.add_event_detect(4, GPIO.FALLING, callback=interrupt_fired)
 
 while(True):
     time.sleep(1)
-    print("timer fired")
+    a = 0
+    data = [{
+        'measurement' : 'pir',        
+        'tags':{
+            'VisionUni' : '2410',
+        },
+        'fields':{
+            'pir' : a,
+        }
+    }]
+    client = None
+    try:
+        client = influxdb('localhost',8086,'root','root','pir')
+    except Exception as e:
+        print "Exception" + str(e)
+    if client is not None:
+        try:
+            client.write_points(data)
+        except Exception as e:
+            print "Exception write " + str(e)
+        finally:
+            client.close()
+    print("running influxdb OK")
+
 
