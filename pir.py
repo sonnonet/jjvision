0a23124678f1c157fae47ec162e7694ec3e4dbb1
qo7860vm <qo7860vm@naver.com>
2020-07-09 16:57:19 +0900

    uptodate

--- pir.py
+++ pir.py
@@ -10,13 +10,34 @@ GPIO.setup(4, GPIO.IN)
 
 def interrupt_fired(channel):
     print("interrupt Fired")
-    a = 1
+    a = 5
+    data = [{
+        'measurement' : 'pir',        
+        'tags':{
+            'VisionUni' : '2410',
+        },
+        'fields':{
+            'pir' : a,
+        }
+    }]
+    client = None
+    try:
+        client = influxdb('localhost',8086,'root','root','pir')
+    except Exception as e:
+        print "Exception" + str(e)
+    if client is not None:
+        try:
+            client.write_points(data)
+        except Exception as e:
+            print "Exception write " + str(e)
+        finally:
+            client.close()
     print(a)
 GPIO.add_event_detect(4, GPIO.FALLING, callback=interrupt_fired)
 
 while(True):
     time.sleep(1)
-    a = 0
+    a = 1
     data = [{
         'measurement' : 'pir',        
         'tags':{
